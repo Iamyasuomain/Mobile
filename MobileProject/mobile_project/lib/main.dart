2b5219a21f2452f8409981a10bfcefc8a95717aa
Iamyasuomain <johncekas20@gmail.com>
2025-04-10 02:58:18 +0700

    feat: create navbarbottom

--- MobileProject/mobile_project/lib/main.dart
+++ MobileProject/mobile_project/lib/main.dart
@@ -1,5 +1,8 @@
 // ignore_for_file: non_constant_identifier_names
-
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'pages/login.dart';
 import 'pages/register.dart';
@@ -11,9 +14,9 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Register(),
+      home: Page(),
     );
   }
 }
@@ -30,7 +33,7 @@ PreferredSizeWidget AppBarCustom(String title) {
       ),
       child: Container(
         height: 150,
-        margin: EdgeInsets.only(top: 50),
+        margin: const EdgeInsets.only(top: 50),
         child: Stack(
           children: [
             Center(
@@ -40,10 +43,14 @@ PreferredSizeWidget AppBarCustom(String title) {
                         fontSize: 30,
                         fontWeight: FontWeight.bold))),
             Positioned(
-              top: -13,
+              top: -10,
               right: 10,
               child: IconButton(
-                icon: Icon(Icons.settings, color: Colors.white, size: 25,),
+                icon: const Icon(
+                  Icons.settings,
+                  color: Colors.white,
+                  size: 25,
+                ),
                 onPressed: () {},
               ),
             )
@@ -52,4 +59,85 @@ PreferredSizeWidget AppBarCustom(String title) {
       ),
     ),
   );
+}
+
+class Page extends StatefulWidget {
+  const Page({super.key});
+
+  @override
+  State<Page> createState() => _PageState();
+}
+
+class _PageState extends State<Page> {
+  int _selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+return Scaffold(
+  bottomNavigationBar: ClipRRect(
+    borderRadius: const BorderRadius.vertical(top: Radius.circular(20)),
+    child: NavigationBarTheme(
+      data: NavigationBarThemeData(
+        indicatorColor: const Color.fromARGB(255, 30, 55, 43),
+        backgroundColor: const Color(0xFF2F5241),
+        iconTheme: WidgetStateProperty.resolveWith<IconThemeData>(  //this just change icon color base on widget state 
+          (Set<WidgetState> states) {
+            if (states.contains(WidgetState.selected)) {
+              return const IconThemeData(color: Color(0xFFFFFFE2)); 
+            }
+            return const IconThemeData(color: Color(0xFFD6CFB9)); 
+          },
+        ),
+        labelTextStyle: WidgetStateProperty.resolveWith<TextStyle>(
+          (Set<WidgetState> states) {
+            if (states.contains(WidgetState.selected)) {
+              return const TextStyle(color: Color(0xFFFFFFE2), fontWeight: FontWeight.bold); 
+            }
+            return const TextStyle(color: Color(0xFFD6CFB9)); 
+          },
+        ),
+      ),
+      child: NavigationBar(
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.photo_camera_outlined),
+            label: "Photo",
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.stacked_bar_chart),
+            label: "Stat",
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.phonelink_setup_outlined),
+            label: "Device",
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.check_box_outlined),
+            label: "Status",
+          ),
+        ],
+        selectedIndex: _selectedIndex,
+        onDestinationSelected: (int index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+      ),
+    ),
+  ),
+  body: IndexedStack(
+    index: _selectedIndex,
+    children: const [
+      Login(),
+      Register(),
+      Placeholder(),
+      Placeholder(),
+      Placeholder(),
+    ],
+  ),
+);
+  }
 }
