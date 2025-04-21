63dffe2cc5a4290363da8a17db435daa40e094dc
Iamyasuomain <johncekas20@gmail.com>
2025-04-21 19:27:40 +0700

    delete Unused

--- MobileProject/mobile_project/lib/services/upload.dart
+++
@@ -1,9 +0,0 @@
-import 'package:image_picker/image_picker.dart';
-pickImage(ImageSource source) async{
-  final ImagePicker picker = ImagePicker();
-  XFile? _file = await picker.pickImage(source: source);
-  if(_file != null){
-    return await _file.readAsBytes();
-  }
-  print('No image Selected');
-}
